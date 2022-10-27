9e0415eb018f85e4d49197e96c7e3b54b1fb93d6
Theo Kostelijk <tkostelijk@kostoso.com>
2022-10-27 10:07:57 +0200

    Running for all subscriptions

--- Log-RunningVM/run.ps1
+++ Log-RunningVM/run.ps1
@@ -9,44 +9,55 @@ if ($Timer.IsPastDue) {
     Write-Host "PowerShell timer is running late!"
 }
 
-# Write an information log with the current time.
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
-
-
 ################################################## Sample Script ###########################################################
 
-# Function App needs to logon with MI to be able to query subscriptions
 $DceURI = "https://logrunningvm-oyua.westeurope-1.ingest.monitor.azure.com"
 $DcrImmutableId = "dcr-c1ee038a22ac48edaf8ae4271dca8d79"
 $Table = "RunningVMs_CL"
+
+# Function App needs to logon with MI to be able to query subscriptions
 Connect-AzAccount -Identity
 
-    $resourceURI = "OperationalInsights"
-    $accessToken = Get-AzAccessToken -ResourceTypeName $resourceURI
+# Get Bearer token
+$resourceURI = "https://monitor.azure.com/"
+$accessToken = get-azaccesstoken -ResourceUrl $resourceURI
+
+
+$subscriptions = Get-AzSubscription
+
+foreach($subscription in $subscriptions){
+
+    Set-AzContext -subscriptionId $subscription.Id
 
     $runningVMs = get-azvm -Status|where-object PowerState -eq "VM Running"|Select-Object -Property * -ExpandProperty HardwareProfile|Group-Object VmSize|Select-Object -Property Name, Count
+
+    # Pricing info: https://learn.microsoft.com/en-us/rest/api/cost-management/retail-prices/azure-retail-prices
     #$vmSize = (Invoke-WebRequest -Uri "https://prices.azure.com/api/retail/prices?api-version=2021-10-01-preview&currencyCode='EUR'&`$filter=serviceName eq 'Virtual Machines' and armSkuName eq 'Standard_B2s' and armRegionName eq 'westeurope'").Content|ConvertFrom-Json
 
-    ## Generate and send some data
-    foreach ($runningVM in $runningVMs) {
-        # We are going to send log entries one by one with a small delay
-        $log_entry = @{
-            # Define the structure of log entry, as it will be sent
-            TimeGenerated = Get-Date ([datetime]::UtcNow) -Format O
-            Application = "LogGenerator"
-            VmSize = $runningVM.Name
-            Value = $runningVM.Count
+    if($runningVMs){
+        ## Generate and send some data
+        foreach ($runningVM in $runningVMs) {
+            # We are going to send log entries one by one with a small delay
+            $log_entry = @{
+                # Define the structure of log entry, as it will be sent
+                TimeGenerated = Get-Date ([datetime]::UtcNow) -Format O
+                Application = "LogGenerator"
+                VmSize = $runningVM.Name
+                Value = $runningVM.Count
+            }
+            # Sending the data to Log Analytics via the DCR!
+            $body = $log_entry | ConvertTo-Json -AsArray;
+            $headers = @{"Authorization" = "Bearer $($accessToken.token)"; "Content-Type" = "application/json" };
+            $uri = "$DceURI/dataCollectionRules/$DcrImmutableId/streams/Custom-$Table"+"?api-version=2021-11-01-preview";
+            $uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers;
+
+            # Let's see how the response looks like
+            Write-Output $uploadResponse
+            Write-Output "---------------------"
         }
-        # Sending the data to Log Analytics via the DCR!
-        $body = $log_entry | ConvertTo-Json -AsArray;
-        $headers = @{"Authorization" = "Bearer $($accessToken.token)"; "Content-Type" = "application/json" };
-        $uri = "$DceURI/dataCollectionRules/$DcrImmutableId/streams/Custom-$Table"+"?api-version=2021-11-01-preview";
-        $uploadResponse = Invoke-RestMethod -Uri $uri -Method "Post" -Body $body -Headers $headers;
+    } 
+}
 
-        # Let's see how the response looks like
-        Write-Output $uploadResponse
-        Write-Output "---------------------"
 
-        # Pausing for 1 second before processing the next entry
-        Start-Sleep -Seconds 1
-    }
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
